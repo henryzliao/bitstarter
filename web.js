42e95ddb12a4e24dd2b29fdabf39036fd83c09b0
Henry Liao <henry@alumni.stanford.edu>
2013-07-10 02:15:21 +0000

    Updated web.js part 1

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+var html_string = fs.readFileSync('index.html');
+console.log("html string is " + html_string);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html_string);
 });
 
 var port = process.env.PORT || 5000;
