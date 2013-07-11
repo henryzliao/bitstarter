698c05923b3b7ccfd3fea3b07e8e0373edaeef7b
Henry Liao <henry@alumni.stanford.edu>
2013-07-11 03:23:14 +0000

    finished grader.js for HW3

--- grader.js
+++ grader.js
@@ -21,11 +21,15 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var sys = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "http://rocky-lowlands-5657.herokuapp.com/broken";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +69,31 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    //console.log(program.file);
+    if(program.url != null)
+    {
+    	var html_data = rest.get(program.url).on('complete', function(result) {
+    	if (result instanceof Error) {
+    		sys.puts('Error: ' + result.message);
+    		this.retry(5000); // try again after 5 sec
+  	} 
+	else {
+    		var htmlfile = "temp.html";
+    		fs.writeFileSync(htmlfile, result)
+		htmlfile = assertFileExists(htmlfile);
+		var checkJson = checkHtmlFile(htmlfile, program.checks);
+        	var outJson = JSON.stringify(checkJson, null, 4);
+        	console.log(outJson);
+	}
+    	});
+    } 
+     else{
+   	var checkJson = checkHtmlFile(program.file, program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+     }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
