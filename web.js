4a944f424186e1729b26a77f96b7407fee1b45ca
Henry Liao <henry@alumni.stanford.edu>
2013-07-23 03:56:59 +0000

    updated web.js to course hw4

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
-var html_string = fs.readFileSync('index.html');
-var buf = new Buffer(html_string, 'utf-8');
-console.log("buffer is " + buf);
+var htmlfile = 'index.html';
+//var buf = new Buffer(html_string, 'utf-8');
+//console.log("buffer is " + buf);
 
 //var buffer = new Buffer(html_string, "utf-8");
 //buffer.write(html_string,"utf-8");
@@ -13,10 +13,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
+//  response.send(buf);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
